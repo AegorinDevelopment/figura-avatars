cff69e8807a0749da41e464aae4f51af60a23b59
Valaron-Jakob <52544138+Valaron-Jakob@users.noreply.github.com>
2024-06-10 00:02:01 +0200

    Refined color matching

--- ChatPreview/script.lua
+++ ChatPreview/script.lua
@@ -6,30 +6,66 @@
 -- Edit the following variables to your liking
 -- -------------------------------------------
 
+-- The default color to apply to the chat preview
+DEFAULT_COLOR = "§7"
+
+-- The formatting rules to apply to the chat preview
+FORMATTINGS = {
+    { matcher = "%* ", replacer = "§6*" .. DEFAULT_COLOR .. " " },
+    { matcher = "%*", replacer = "§6*" },
+    { matcher = "%]", replacer = "§9]" .. DEFAULT_COLOR },
+    { matcher = "%[", replacer = "§9[" },
+    { matcher = "%)", replacer = "§8)" .. DEFAULT_COLOR },
+    { matcher = "%(", replacer = "§8(" },
+    { matcher = "| ", replacer = "§4" .. DEFAULT_COLOR .. " " },
+    { matcher = "|", replacer = "§4" },
+}
+
+-- The interval in which the chat preview is updated in ticks
+TICK_INTERVAL = 5
+
+
 -- ! Do not change anything below this line !
 -- ------------------------------------------
 
+CHAT_PREVIEW = models:newPart("ChatPreview", "HUD"):newText("")
 
-local chat_preview = models:newPart("ChatPreview", "HUD"):newText("")
+CHAT_PREVIEW:setBackgroundColor(0, 0, 0, 0.5)
+CHAT_PREVIEW:setShadow(true)
 
 
-function FormatText(text, matcher, replacer)
-    return text:gsub("%s%*", " §6*"):gsub("%*%s", "*§7 ")
+function FormatText(text, format)
+    return text:gsub(format.matcher, format.replacer)
+end
+
+function FormatAllTexts(text, formats)
+    for _, format in ipairs(formats) do
+        text = FormatText(text, format)
+    end
+    return text
 end
 
 
+local counter = 0
+
 function events.TICK()
+    if counter > 0 then
+        counter = counter - 1
+        return
+    else
+        counter = TICK_INTERVAL
+    end
+
     if not host:isChatOpen() then 
-        chat_preview:setVisible(false)
+        CHAT_PREVIEW:setVisible(false)
         return 
     end
 
-    local window_size = client:getScaledWindowSize()
+    WINDOW_SIZE_SCALED = client:getScaledWindowSize()
 
-    chat_preview:setPos(-3, -window_size.y+37, 1000)
-    chat_preview:setWidth(window_size.x-6)
-    chat_preview:setBackgroundColor(0, 0, 0, 0.5)
+    CHAT_PREVIEW:setPos(-3, -WINDOW_SIZE_SCALED.y + 37, -99)
+    CHAT_PREVIEW:setWidth(WINDOW_SIZE_SCALED.x - 6)
 
-    chat_preview:setVisible(true)
-    chat_preview:setText(FormatText("§7" .. host:getChatText()))
+    CHAT_PREVIEW:setVisible(true)
+    CHAT_PREVIEW:setText(FormatAllTexts("§7" .. host:getChatText(), FORMATTINGS))
 end
