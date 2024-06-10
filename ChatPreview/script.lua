3e4f8918b465550d7e5802e7215ba24feeab1d6d
Valaron-Jakob <52544138+Valaron-Jakob@users.noreply.github.com>
2024-06-10 23:24:47 +0200

    Added positioning

--- ChatPreview/script.lua
+++ ChatPreview/script.lua
@@ -4,6 +4,44 @@
     Using the `FORMATTINGS` variable you can define custom rules to format the chat text.
 ]]
 
+-- Edit the following variables to your liking
+-- -------------------------------------------
+
+--- Tha place where the chat preview is displayed. You can use the following values:
+--- - `Top`: Display the preview on top of the screen
+--- - `Bottom`: Display the preview right above the chat input
+--- @type string
+DISPLAY_LOCATION = "Bottom"
+
+--- The default color to apply to the chat preview (use `§` for MC color codes)
+--- @type color
+DEFAULT_COLOR = "§7"
+
+--- The formatting rules to apply to the chat preview. You can add your own rules by extending
+--- the list.
+--- - `matcher`: The pattern to search for in the chat text (https://www.lua.org/pil/20.2.html)
+--- - `replacer`: The string to replace the matched pattern with (use `DEFAULT_COLOR` to reset)
+--- @type format[]
+FORMATTINGS = {
+    { matcher = "%* ", replacer = "§3*" .. DEFAULT_COLOR .. " " },
+    { matcher = "%*", replacer = "§3*" },
+    { matcher = "%]", replacer = "§9]" .. DEFAULT_COLOR },
+    { matcher = "%[", replacer = "§9[" },
+    { matcher = "%)", replacer = "§8)" .. DEFAULT_COLOR },
+    { matcher = "%(", replacer = "§8(" },
+    { matcher = "| ", replacer = "§4" .. DEFAULT_COLOR .. " " },
+    { matcher = "|", replacer = "§4" },
+    { matcher = "%>", replacer = "§e>" },
+}
+
+--- The interval in which the chat preview is updated (in ticks)
+--- @type integer
+UPDATE_INTERVAL = 5
+
+
+-- ! Do not change anything below this line !
+-- ------------------------------------------
+
 ---@alias color
 ---| '"§0"' # Black
 ---| '"§1"' # Dark Blue
@@ -30,35 +68,6 @@
 
 ---@alias format { matcher: string, replacer: string }
 
--- Edit the following variables to your liking
--- -------------------------------------------
-
---- The default color to apply to the chat preview (use `§` for MC color codes)
---- @type color
-DEFAULT_COLOR = "§7"
-
---- The formatting rules to apply to the chat preview. You can add your own rules by extending
---- the list.
---- - `matcher`: The pattern to search for in the chat text (https://www.lua.org/pil/20.2.html)
---- - `replacer`: The string to replace the matched pattern with (use `DEFAULT_COLOR` to reset)
---- @type format[]
-FORMATTINGS = {
-    { matcher = "%* ", replacer = "§3*" .. DEFAULT_COLOR .. " " },
-    { matcher = "%*", replacer = "§3*" },
-    { matcher = "%]", replacer = "§9]" .. DEFAULT_COLOR },
-    { matcher = "%[", replacer = "§9[" },
-    { matcher = "%)", replacer = "§8)" .. DEFAULT_COLOR },
-    { matcher = "%(", replacer = "§8(" },
-    { matcher = "| ", replacer = "§4" .. DEFAULT_COLOR .. " " },
-    { matcher = "|", replacer = "§4" },
-}
-
---- The interval in which the chat preview is updated (in ticks)
---- @type integer
-UPDATE_INTERVAL = 5
-
--- ! Do not change anything below this line !
--- ------------------------------------------
 
 CHAT_PREVIEW = models:newPart("ChatPreview", "HUD"):newText("")
 
@@ -66,22 +75,22 @@ CHAT_PREVIEW:setBackgroundColor(0, 0, 0, 0.5)
 CHAT_PREVIEW:setShadow(true)
 
 --- Formats a text using a given format table
---- @param self string
+--- @param text string
 --- @param format format
 --- @return string, integer
-function string:customFormat(format)
-    return self:gsub(format.matcher, format.replacer)
+function CustomFormat(text, format)
+    return text:gsub(format.matcher, format.replacer)
 end
 
 --- Formats a text using a list of format tables
---- @param self string
+--- @param text string
 --- @param formats format[]
 --- @return string
-function string:customFormatAll(formats)
+function CustomFormatAll(text, formats)
     for _, format in ipairs(formats) do
-        self:customFormat(format)
+        text = CustomFormat(text, format)
     end
-    return self
+    return text
 end
 
 
@@ -101,12 +110,19 @@ function events.TICK()
         return
     end
 
+    -- Define the chat preview position and size
     WINDOW_SIZE_SCALED = client:getScaledWindowSize()
+    if DISPLAY_LOCATION == "Top" then
+        CHAT_PREVIEW:setScale(1)
+        CHAT_PREVIEW:setPos(-3, -3, -99)
+        CHAT_PREVIEW:setWidth(WINDOW_SIZE_SCALED.x - 6)
+    else
+        CHAT_PREVIEW:setScale(0.8)
+        CHAT_PREVIEW:setPos(-3, -WINDOW_SIZE_SCALED.y + 38, -99)
+        CHAT_PREVIEW:setWidth((WINDOW_SIZE_SCALED.x - 6) / 0.8)
+    end
 
-    CHAT_PREVIEW:setPos(-3, -3, -99)
-    -- CHAT_PREVIEW:setPos(-3, -WINDOW_SIZE_SCALED.y + 37, -99)
-    CHAT_PREVIEW:setWidth(WINDOW_SIZE_SCALED.x - 6)
-
+    -- Update the chat preview text
     CHAT_PREVIEW:setVisible(true)
-    CHAT_PREVIEW:setText((DEFAULT_COLOR .. host:getChatText()):customFormatAll(FORMATTINGS))
+    CHAT_PREVIEW:setText(CustomFormatAll(DEFAULT_COLOR .. host:getChatText(), FORMATTINGS))
 end
