1f54f983c1f9a3b5ac5227b9b6599ca987b85836
Valaron-Jakob <52544138+Valaron-Jakob@users.noreply.github.com>
2024-06-13 14:13:14 +0200

    Updated to enable custom modificators per key

--- AnimKeys/script.lua
+++ AnimKeys/script.lua
@@ -6,26 +6,28 @@
 -- Edit the following variables to your liking
 -- -------------------------------------------
 
---- List of keybinds to toggle different animations. Change to your liking.
+--- List of keybinds on your numpad to toggle different animations. Change to your liking.
+--- - `key` - Set to whatever button you want to press to toggle the animation.
+--- - `mod` - Set the modifier, that needs to be pressed alongside the keybind to
+---         toggle the animations. Common values are: (Numbers)
+---         | 0 for no Modifier
+---         | 1 for `⇧ Shift`
+---         | 2 for `✲ Ctrl`
+---         | 4 for `⎇ Alt`
+--- - `name` - Set to the name your animation should be shown as
+--- - `anim` - Set to the animation you want to toggle
 --- @type keybind[]
 KEYBINDS = {
-    { key = "key.keyboard.0", name = "Sitting", anim = "pose.sitting" },
-    { key = "key.keyboard.1", name = "Standing", anim = "pose.standing" },
+    { key = "key.keyboard.keypad.0", mod = 0, name = "Sitting", anim = "pose.sitting" },
+    { key = "key.keyboard.keypad.1", mod = 0, name = "Standing", anim = "pose.standing" },
+    { key = "key.keyboard.keypad.1", mod = 2, name = "Standing 2", anim = "pose.standing.2" },
 }
-
---- Set the modifier key, that needs to be pressed alongside the keybind to
---- toggle the animations. Common values are: (Numbers)
---- | 1 for `⇧ Shift`
---- | 2 for `✲ Ctrl`
---- | 4 for `⎇ Alt`
---- @type Event.Press.modifiers
-MODIFIER = 1
 
 
 -- ! Do not change anything below this line !
 -- ------------------------------------------
 
---- @alias keybind {key: string, name: string, anim: string}
+--- @alias keybind {key: Minecraft.keyCode, mod: Event.Press.modifiers, name: string, anim: string}
 
 --- @type table<string, boolean>
 local keyStates = {}
@@ -37,7 +39,7 @@ for _, keybind in ipairs(KEYBINDS) do
 
     -- Create on press event
     key.press = function(modifier)
-        if modifier ~= MODIFIER then return end
+        if modifier ~= keybind.mod then return end
 
         -- Turn off all states
         for _, value in pairs(keyStates) do
@@ -54,7 +56,7 @@ end
 --- @param states table<string, boolean>
 function pings.animPing(states)
     if not player:isLoaded() then return end
-    
+
     for anim, state in pairs(states) do
         animations.base[anim]:setPlaying(state)
     end
