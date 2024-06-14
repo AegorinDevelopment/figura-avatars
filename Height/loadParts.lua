6d309625cd9a29e757fac480939b64cb334dd04d
Valaron-Jakob <52544138+Valaron-Jakob@users.noreply.github.com>
2024-06-14 18:35:47 +0200

    Converted to lib

---
+++ Height/loadParts.lua
@@ -0,0 +1,40 @@
+
+
+local lib = {}
+
+--- Converts modelparts to an indexed table
+--- @param model ModelPart
+--- @return table<string, ModelPart>
+local function getModelChildsAssoc(model)
+    local children = model:getChildren()
+    local childTable = {}
+
+    for _, child in pairs(children) do
+        childTable[child:getName()] = child
+    end
+
+    return childTable
+end
+
+--- Loads custom parts and moves them to the correct position
+function lib.loadCustomParts()
+    local defaultBones = getModelChildsAssoc(models.base.root)
+    local parts = getModelChildsAssoc(models)
+    parts["base"] = nil
+
+    for _, part in pairs(parts) do
+        local bones = part:getChildren()
+
+        for _, bone in pairs(bones) do
+            if defaultBones[bone:getName()] ~= nil then
+                local subBones = bone:getChildren()
+
+                for _, subBone in pairs(subBones) do
+                    subBone:moveTo(defaultBones[bone:getName()])
+                end
+            end
+        end
+    end
+end
+
+return lib
