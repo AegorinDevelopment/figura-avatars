5a0a9b31efcd6f5c6d085dbad39d80b7019be590
Valaron-Jakob <52544138+Valaron-Jakob@users.noreply.github.com>
2024-09-05 19:48:04 +0200

    Fixed animations sending huge pings

--- AvatarRPBase/scripts/animKeys.lua
+++ AvatarRPBase/scripts/animKeys.lua
@@ -44,39 +44,40 @@ KEYBINDS = {
 --- @alias keybind {key: Minecraft.keyCode, mod: Event.Press.modifiers, name: string, anim: string}
 
 --- @type table<string, boolean>
-local keyStates = {}
+local animStates = {}
 
 --- Creates new keybinds and saves all of them in a table to index them later
 for _, keybind in ipairs(KEYBINDS) do
     local key = keybinds:newKeybind(keybind.name, keybind.key, false)
-    keyStates[keybind.anim] = false
+    animStates[keybind.anim] = false
 
     -- Create on press event
     key.press = function(modifier)
         if modifier ~= keybind.mod then return end
 
-        local keyNew = not keyStates[keybind.anim]
-
-        -- Turn off all states
-        for _, value in pairs(keyStates) do
-            keyStates[_] = false
-        end
+        local keyNew = not animStates[keybind.anim]
 
         -- Switch only given keyState
-        keyStates[keybind.anim] = keyNew
-        pings.animPing(keyStates)
+        pings.animPing(keybind.anim, keyNew)
 
-        -- log(keybind.name .. " " .. (keyStates[keybind.anim] and "enabled" or "disabled"))
+        -- log(keybind.name .. " " .. (animStates[keybind.anim] and "enabled" or "disabled"))
     end
 end
 
---- Updates all animations
---- @param states table<string, boolean>
-function pings.animPing(states)
+--- Turns off all animations and only updates the given one using the state or false if not given
+--- @param anim string The animation to update
+--- @param state boolean The state of the animation
+function pings.animPing(anim, state)
     if not player:isLoaded() then return end
 
-    for anim, state in pairs(states) do
-        animations.avatar[anim]:setPlaying(state)
+    for key in pairs(animStates) do
+        if key == anim then
+            animStates[key] = state
+        else
+            animStates[key] = false
+        end
+
+        animations.avatar[key]:setPlaying(animStates[key])
     end
 end
 
@@ -95,5 +96,9 @@ events.TICK:register(function()
         anim_timer = ANIM_INTERVAL
     end
 
-    pings.animPing(keyStates)
+    for anim, state in pairs(animStates) do
+        if state then
+            pings.animPing(anim, state)
+        end
+    end
 end, "animUpdate")
